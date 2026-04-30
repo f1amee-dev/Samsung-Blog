b4190c2914225b852bf54520fa5841c08d02fe3c
Filip Persuric Bernobic <filipperberp@gmail.com>
2026-04-30 13:39:00 +0200

    site title + opis

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// site-wide strings, used in the head and a couple of components
+
+export const SITE_TITLE = 'Iza Razglednice';
+export const SITE_DESCRIPTION =
+	'Studentski portfolio o turizmu, mladima i budućnosti Istre, u sklopu Samsung Izazova.';
