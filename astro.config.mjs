1800bc35588d2cd98db55b8d1d66093843550a41
Filip Persuric Bernobic <filipperberp@gmail.com>
2026-04-30 19:58:00 +0200

    responsive image config + breakpoints

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	image: {
+		responsiveStyles: true,
+		layout: 'constrained',
+		breakpoints: [640, 750, 828, 1080, 1280, 1668, 2048],
+	},
 });
